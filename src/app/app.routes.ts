3b2f5ba40bc3655f13e8fb8102f6d2e1627ddd79
a1x40v <a1x40v@gmail.com>
2024-08-11 21:57:55 +0300

    Navigate to dynamic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
   {
@@ -9,7 +9,7 @@ export const routes: Routes = [
     component: NoTaskComponent,
   },
   {
-    path: 'tasks', // <domain>/tasks
-    component: TasksComponent,
+    path: 'users/:userId', // <domain>/users/<uid>
+    component: UserTasksComponent,
   },
 ];
