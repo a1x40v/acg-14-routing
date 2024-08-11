3b9772e8b408238d35924340d051e7ba7cc57baf
a1x40v <a1x40v@gmail.com>
2024-08-11 21:36:04 +0300

    Render routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { TasksComponent } from './tasks/tasks.component';
 
 export const routes: Routes = [
+  {
+    path: '', // <domain>/
+    component: NoTaskComponent,
+  },
   {
     path: 'tasks', // <domain>/tasks
     component: TasksComponent,
